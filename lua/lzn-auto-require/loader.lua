ead8819dc2c06866f3e59e0a0fd9d1e0db6e24e6
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2024-08-04 01:08:00 +0200

    fixed loader

--- lua/lzn-auto-require/loader.lua
+++ lua/lzn-auto-require/loader.lua
@@ -5,29 +5,37 @@ local M = {}
 ---@return boolean found,  string|string[] pathOrTriedPaths, string? pack_name
 local function find_opt_file(relPaths)
 	local triedPaths = {}
-	for _, pack in ipairs(vim.opt.packpath:get()) do
-		local optPath = vim.fs.joinpath(pack, 'opt')
-		if vim.fn.isdirectory(optPath) == 0 then
+	-- I need to look for {pack}/pack/*/opt/{pack_name}/lua/...
+	for _, packpath in ipairs(vim.opt.packpath:get()) do
+		local groupsPath = vim.fs.joinpath(packpath, 'pack')
+		if vim.fn.isdirectory(groupsPath) == 0 then
 			goto continue
 		end
 
-		for plugDir, typ in vim.fs.dir(optPath) do
-			local pluginPath = vim.fs.joinpath(optPath, plugDir)
+		for group, typ in vim.fs.dir(groupsPath) do
 			if typ ~= "directory" then
-				goto continue2
+				goto continue3
 			end
 
-			for _, relPath in ipairs(relPaths) do
-				local fullPath = vim.fs.joinpath(pluginPath, relPath)
-				if vim.fn.filereadable(fullPath) then
-					print('full', fullPath, 'pack_name', plugDir, 'plugPath', pluginPath)
-					return true, fullPath, plugDir
+			local groupPath = vim.fs.joinpath(groupsPath, group)
+			for pack_name, typ in vim.fs.dir(groupPath .. '/opt') do
+				if typ ~= "directory" then
+					goto continue4
 				end
 
-				table.insert(triedPaths, fullPath)
-			end
+				local pack_path = vim.fs.joinpath(groupPath, 'opt', pack_name)
+				for _, relPath in ipairs(relPaths) do
+					local fullPath = vim.fs.joinpath(pack_path, relPath)
+					if vim.fn.filereadable(fullPath) == 1 then
+						return true, fullPath, pack_name
+					end
 
-			::continue2::
+					table.insert(triedPaths, fullPath)
+				end
+
+				::continue4::
+			end
+			::continue3::
 		end
 		::continue::
 	end
